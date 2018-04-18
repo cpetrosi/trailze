c6e2921920bfff64f35ac1e0645aa040af1ace0b
Carleen <cpetrosi@lion.lmu.edu>
2018-04-17 19:45:36 -0700

    fixing tab navigation

--- app/App.js
+++ app/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { View } from 'react-native';
+import { TabNavigator } from 'react-navigation';
 import SearchBar from './components/SearchBar';
 import AppHeader from './components/AppHeader';
+import MapViews from './components/MapViews';
 
 const API_KEY = 'AIzaSyAmsdTMY6U_Ac5i21bYQaPRpesyG7o4cpY';
 
-export default class App extends React.Component {
+class SearchScreen extends React.Component {
   onPressSearch = term => {
     console.log(term);
   }
-
   render() {
     return (
       <View style={{ flex: 1, backgroundColor: '#ddd' }}>
@@ -23,3 +24,21 @@ export default class App extends React.Component {
     );
   }
 }
+
+class MapScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, backgroundColor: '#ddd' }}>
+        <AppHeader
+          headerText = "Trailze"
+        />
+        <MapViews/>
+      </View>
+    );
+  }
+}
+
+export default TabNavigator({
+  Search: { screen: SearchScreen },
+  Map: { screen: MapScreen },
+});
--- app/components/AppHeader.js
+++ app/components/AppHeader.js
@@ -3,10 +3,8 @@ import { Header } from 'react-native-elements';
 
 const AppHeader = ({ headerText }) => (
   <Header
-    leftComponent={{ icon: 'menu', color: '#fff' }}
     centerComponent={{ text: headerText, style: {color: 'white'} }}
     outerContainerStyles={{ backgroundColor: '#cdaf95' }}
-    rightComponent={{ icon: 'map', color: '#fff' }}
   />
 );
 
